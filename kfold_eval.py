3e0fd1c2a79f6271fde6d7dea946361149299126
Li Pan <847041172@qq.com>
2021-09-16 18:17:23 +0800

    Add files via upload

---
+++ kfold_eval.py
@@ -0,0 +1,236 @@
+"""
+10-fold cross-validation of MLP and GCN
+"""
+import os
+import time
+import torch
+import pandas as pd
+import numpy as np
+from training import train_mlp, test_mlp, train_gcn, test_gcn
+from models import GPModel, MultilayerPerceptron, GCN
+from sklearn.model_selection import KFold
+from torch.utils.data import Subset
+from torch_geometric.data import DataLoader, Data
+from sklearn.linear_model import LogisticRegression
+
+
+def kfold_mlp(data, args):
+    """
+    10-fold cross-validation of MultilayerPerceptron.
+    Note that another 10-fold is implemented on the train set.
+    Remember not access the test set at this stage. Some parameters are locally set here.
+    :param data: pooling results.
+    :param args: args from main.py
+    :return: None. Best model for each fold is saved to args.check_dir/MLP/fold_%d
+    """
+    # locally set some parameters
+    args.times = 5  # repeat times of the second level 10-fold cross-validation
+    args.least = 50  # smallest number of training epochs; avoid under-fitting
+    args.patience = 50  # patience for early stopping
+    args.epochs = 1000  # maximum number of epochs
+    args.weight_decay = 0.1
+
+    indices = np.arange(871)
+    kf = KFold(n_splits=10, random_state=args.seed, shuffle=True)
+    # as state in the paper, we run another 10-fold cross-validation
+    val_kf = KFold(n_splits=10, shuffle=True)
+
+    x = data[:, :-1]
+    y = data[:, -1]
+    x = torch.tensor(x, dtype=torch.float)
+    y = torch.tensor(y, dtype=torch.long)
+    dataset = torch.utils.data.TensorDataset(x, y)
+    args.num_features = x.shape[1]
+
+    # repeat args.times times
+    # the larger this parameter, the more stable the performance
+    for repeat in range(args.times):
+        print('%d times CV out of %d on training MLP...' % (repeat + 1, args.times))
+        for i, (train_idx, test_idx) in enumerate(kf.split(indices)):
+            print('Training MLP on the %d fold...' % (i + 1))
+            for count, (train_id, val_id) in enumerate(val_kf.split(train_idx)):
+                if args.verbose:
+                    print('%d val set out of 10' % (count + 1))
+
+                # create fold dir
+                fold_dir = os.path.join(args.check_dir, 'MLP', 'fold_%d' % (i + 1))
+                if not os.path.exists(fold_dir):
+                    os.makedirs(fold_dir)
+
+                # Make sure the test indices are the same
+                # for every time you run the K fold
+                # it's silly but I must make sure
+                test_log = os.path.join(fold_dir, 'test_indices.txt')
+                if not os.path.exists(test_log):
+                    np.savetxt(test_log, test_idx, fmt='%d', delimiter=' ')
+                else:
+                    saved_indices = np.loadtxt(test_log, dtype=int)
+                    assert np.array_equal(test_idx, saved_indices), \
+                        'Something goes wrong with 10-fold cross-validation'
+
+                # set model
+                model = MultilayerPerceptron(args).to(args.device)
+                opt = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+                # Set training and validation set
+                # Do not use the test set
+                train_set = Subset(dataset, train_id)
+                val_set = Subset(dataset, val_id)
+
+                # Build the data loader
+                training_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+                validation_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False)
+
+                # Model training
+                best_model, best_val_acc, best_val_loss = train_mlp(model=model, train_loader=training_loader,
+                                                                    val_loader=validation_loader, optimizer=opt,
+                                                                    save_path=fold_dir, args=args)
+
+                # Load the best model for validation set
+                checkpoint = torch.load(os.path.join(fold_dir, '{}.pth'.format(best_model)))
+                model.load_state_dict(checkpoint['net'])
+
+                # Save the best model
+                state = {'net': model.state_dict(), 'args': args}
+                torch.save(state, os.path.join(fold_dir, 'num_{:d}_valacc_{:.6f}_pool_{:.3f}_epoch_{:d}_.pth'
+                                               .format(count + 1, best_val_acc, args.pooling_ratio, best_model)))
+
+
+def kfold_gcn(edge_index, edge_attr, args):
+    """
+    Training phase of GCN. Some parameters of args are locally set here.
+    No validation is implemented in this section.
+    :param edge_index: adjacency matrix of population graph.
+    :param edge_attr: edge weights, say cosine similarity values
+    :param args: args from main.py
+    :return:
+    """
+    # locally set parameters
+    args.num_features = args.nhid // 2  # output feature size of MLP
+    args.nhid = args.num_features // 2
+    args.epochs = 10000  # maximum number of training epochs
+    args.patience = 5000  # patience for early stop regarding the performance on train set
+    args.weight_decay = 0.001
+
+    # load population graph
+    edge_index = torch.tensor(edge_index, dtype=torch.long)
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float)
+
+    # silly hard coding..
+    indices = np.arange(871)
+    kf = KFold(n_splits=10, shuffle=True, random_state=args.seed)
+
+    # store the predictions
+    result_df = pd.DataFrame([])
+    test_result_acc = []
+    test_result_loss = []
+
+    for i, (train_idx, test_idx) in enumerate(kf.split(indices)):
+        # Ready to read further learned features extracted by MLP on different folds
+        fold_path = os.path.join(args.data_dir, 'Further_Learned_Features', 'fold_%d' % (i + 1))
+        # working path of training gcn
+        work_path = os.path.join(args.check_dir, 'GCN')
+        if not os.path.exists(work_path):
+            os.makedirs(work_path)
+
+        print('Training GCN on the %d fold' % (i + 1))
+        model = GCN(args).to(args.device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+        feature_path = os.path.join(fold_path, 'features.txt')
+        assert os.path.exists(feature_path), \
+            'No further learned features found!'
+        content = pd.read_csv(feature_path, header=None, sep='\t')
+        x = content.iloc[:, :-1].values
+        y = content.iloc[:, -1].values
+
+        x = torch.tensor(x, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.long)
+        data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+
+        # form the mask from idx
+        train_mask = np.zeros(871)
+        test_mask = np.zeros(871)
+        train_mask[train_idx] = 1
+        test_mask[test_idx] = 1
+
+        # set the mask for dataset
+        data.train_mask = torch.tensor(train_mask, dtype=torch.bool)
+        data.test_mask = torch.tensor(test_mask, dtype=torch.bool)
+
+        loader = DataLoader([data], batch_size=args.batch_size)
+
+        # Model training
+        best_model = train_gcn(loader, model, optimizer, work_path, args)
+        # Restore best model for test set
+        checkpoint = torch.load(os.path.join(work_path, '{}.pth'.format(best_model)))
+        model.load_state_dict(checkpoint['net'])
+        test_acc, test_loss, test_out = test_gcn(loader, model, args)
+        result_df['fold_%d' % (i + 1)] = test_out
+        test_result_acc.append(test_acc)
+        test_result_loss.append(test_loss)
+        print('GCN {:0>2d} fold test set results, loss = {:.6f}, accuracy = {:.6f}'.format(i + 1, test_loss, test_acc))
+
+        state = {'net': model.state_dict(), 'args': args}
+        torch.save(state, os.path.join(work_path, 'fold_{:d}_test_{:.6f}_drop_{:.3f}_.pth'
+                                       .format(i + 1, test_acc, args.dropout_ratio)))
+
+    # save the predictions to args.result_dir/Graph Convolutional Networks/GCN_pool_%.3f_seed_%d_.csv
+    result_path = os.path.join(args.result_dir, 'Graph Convolutional Networks')
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    result_df.to_csv(os.path.join(result_path, 'GCN_pool_%.3f_seed_%d_.csv' % (args.pooling_ratio, args.seed)),
+                     index=False, header=True)
+
+
+def logistic(args):
+    kf = KFold(n_splits=10, random_state=args.seed, shuffle=True)
+    indices = np.arange(871)
+    checkpoint_dir = os.path.join(args.data_dir, 'Further_Learned_Features')
+    acc_set = []
+
+    # store the output of logistic regression
+    result_df = pd.DataFrame([])
+
+    time_length = []
+    training_iters = []
+    for i, (train_idx, test_idx) in enumerate(kf.split(indices)):
+        fold_path = os.path.join(checkpoint_dir, 'fold_%d' % (i+1))
+        feature_file = os.path.join(fold_path, 'features.txt')
+        indices_file = os.path.join(fold_path, 'test_indices.txt')
+        saved_indices = np.loadtxt(indices_file, dtype=int)
+
+        # Again, make sure you are using the same test set
+        assert np.array_equal(test_idx, saved_indices), \
+            'Something wrong with the 10-fold Cross Validation'
+
+        print('Evaluate on the %d fold' % (i+1))
+        features = pd.read_csv(feature_file, sep='\t',header=None)
+        x = features.iloc[:, :-1].values
+        y = features.iloc[:, -1].values
+        trainx = x[train_idx]
+        trainy = y[train_idx]
+        testx = x[test_idx]
+        testy = y[test_idx]
+
+        start = time.time()
+        clf = LogisticRegression(max_iter=10000)
+        clf.fit(trainx, trainy)
+        end = time.time()
+        time_length.append(end-start)
+        training_iters.append(clf.n_iter_)
+        test_acc = clf.score(testx, testy)
+        acc_set.append(test_acc)
+        result_df['fold_%d' % (i + 1)] = clf.predict_proba(x)[:, 1]
+
+        print('Test Accuracy: %f' % test_acc)
+
+    print('\nAverage acc: %f' % (sum(acc_set)/len(acc_set)))
+    print('Average Training Overhead (s): %f' % (sum(time_length)/len(time_length)))
+    print('Average Training Iterations: %d' % (sum(training_iters)/len(training_iters)))
+    result_path = os.path.join(args.result_dir, 'Logistic Regression')
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+    result_df.to_csv(os.path.join(result_path, 'LR_pool_%.3f_seed_%d_.csv'%(args.pooling_ratio, args.seed)),
+                     index=False, header=True)
---
+++ training.py
@@ -0,0 +1,342 @@
+"""
+Training procedures of MLP and GCN
+"""
+import os
+import torch
+import time
+import pandas as pd
+import numpy as np
+import shutil
+from torch_geometric.datasets import TUDataset
+from torch_geometric.data import DataLoader
+from models import GPModel, MultilayerPerceptron
+
+import torch.nn as nn
+
+
+def graph_pooling(args):
+    """
+    Run graph pooling on the graph representation of brain imagings from raw ABIDE dataset.
+    This is the first part, say unsupervised graph pooling, in our paper.
+    :param args: args from the main.py
+    :return: None. Pooling results are saved to args.data/ABIDE_downsample
+                    with respect to different pooling ratios
+    """
+    torch.manual_seed(args.seed)
+    # load data
+    abide_dataset = TUDataset(args.data_dir, name='ABIDE', use_node_attr=True)
+    args.num_classes = abide_dataset.num_classes
+    args.num_features = abide_dataset.num_features
+
+    # hierarchical graph pooling model
+    gp = GPModel(args).to(args.device)
+
+    # run graph pooling
+    abide_loader = DataLoader(abide_dataset, batch_size=args.batch_size, shuffle=False)
+    downsample = []
+    label = []
+    for i, data in enumerate(abide_loader):
+        data = data.to(args.device)
+        downsample += gp(data).cpu().detach().numpy().tolist()
+        label += data.y.cpu().detach().numpy().tolist()
+    downsample_df = pd.DataFrame(downsample)
+    # store the label, in case of data samples shuffle
+    downsample_df['label'] = label
+
+    # store the selected substructures
+    downsample_dir = os.path.join(args.data_dir, 'ABIDE_downsample')
+    if not os.path.exists(downsample_dir):
+        os.makedirs(downsample_dir)
+    downsample_file = os.path.join(downsample_dir, 'ABIDE_pool_%.3f_.txt' % args.pooling_ratio)
+    downsample_df.to_csv(downsample_file, index=False, header=False, sep='\t')
+
+    del gp
+    del data
+    del abide_dataset
+    del abide_loader
+    del downsample_df
+    torch.cuda.empty_cache()
+
+
+def test_mlp(model, loader, args):
+    """
+    Test the MLP performance on loader (validation dataloader)
+    :param model: an instance of MultilayerPerceptron
+    :param loader: dataloader, specifically, the validation dataloader.
+    :param args: args from the main.py
+    :return: accuracy, loss on the validation set.
+    """
+    model.eval()
+    correct = 0.0
+    loss_test = 0.0
+    for data_x, data_y in loader:
+        data_x, data_y = data_x.to(args.device), data_y.to(args.device)
+        out, _ = model(data_x)
+        pred = (out > 0).long()
+        correct += pred.eq(data_y).sum().item()
+        loss_func = nn.BCEWithLogitsLoss()
+        loss_test += loss_func(out, data_y.float()).item()
+    return correct / len(loader.dataset), loss_test
+
+
+def train_mlp(model, train_loader, val_loader, optimizer, save_path, args):
+    """
+    Training progress of Multilayer Perceptron.
+    As said in the paper, we have implemented a special training progress,
+    i.e., we have run another 10-fold cross-validation on the given dataset,
+    and select the best model by limiting the smallest number of training epochs,
+    and choosing the one with highest validation accuracy. The test set is strictly hidden.
+    :param model: an instance of MultilayerPerceptron
+    :param train_loader: dataloader of training set
+    :param val_loader: dataloader of test set
+    :param optimizer: Adam, by default
+    :param save_path: temporary working path of this progress
+    :param args: args from main.py
+    :return: best_epoch: name of the best model
+    :return: min_loss, max_accuracy: loss and accuracy on the validation set of the best model
+    """
+    min_loss = 1e10
+    max_acc = 0
+    patience_cnt = 0
+    val_loss_values = []
+    val_acc_values = []
+    best_epoch = 0
+    epoch_num = 0
+
+    t = time.time()
+    model.train()
+    for epoch in range(args.epochs):
+        loss_train = 0.0
+        correct = 0
+        for i, (data_x, data_y) in enumerate(train_loader):
+            optimizer.zero_grad()
+            data_x, data_y = data_x.to(args.device), data_y.to(args.device)
+            out, _ = model(data_x)
+            loss_func = nn.BCEWithLogitsLoss()
+            loss = loss_func(out, data_y.float())
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+            pred = (out > 0).long()
+            correct += pred.eq(data_y).sum().item()
+        acc_train = correct / len(train_loader.dataset)
+        acc_val, loss_val = test_mlp(model, val_loader, args)
+        if args.verbose:
+            print('\r', 'Epoch: {:04d}'.format(epoch + 1), 'loss_train: {:.6f}'.format(loss_train),
+                  'acc_train: {:.6f}'.format(acc_train), 'loss_val: {:.6f}'.format(loss_val),
+                  'acc_val: {:.6f}'.format(acc_val), 'time: {:.6f}s'.format(time.time() - t), end='', flush=True)
+
+        val_loss_values.append(loss_val)
+        val_acc_values.append(acc_val)
+        epoch_num += 1
+        model_state = {'net': model.state_dict(), 'args': args}
+        torch.save(model_state, os.path.join(save_path, '{}.pth'.format(epoch)))
+
+        # Skip logging the first args.least models
+        if epoch_num < args.least:
+            continue
+        if val_loss_values[-1] <= min_loss:
+            min_loss = val_loss_values[-1]
+            max_acc = val_acc_values[-1]
+            best_epoch = epoch
+            patience_cnt = 0
+        else:
+            patience_cnt += 1
+
+        if patience_cnt == args.patience:
+            break
+
+        # delete other models
+        files = [f for f in os.listdir(save_path) if f.endswith('.pth')]
+        for f in files:
+            if f.startswith('num'):
+                continue
+            epoch_nb = int(f.split('.')[0])
+            if epoch_nb != best_epoch:
+                os.remove(os.path.join(save_path, f))
+    if args.verbose:
+        print('\nOptimization Finished! Total time elapsed: {:.6f}'.format(time.time() - t))
+
+    return best_epoch, max_acc, min_loss
+
+
+def extract(data, args):
+    """
+    Herein, we use the best MLP model to extract further learned features from the pooling results
+    This is the one that connects second part(MLP) and third part(GCN or LR) in our paper.
+    :param data: Pooling results. shape: [number of subjects, dim of pooling features] = [871, 378]
+    :param args: args from main.py
+    :return: None. All the extracted further learned features are saved to /args.data/Further_Learned_Features/fold_%d
+    """
+    x = data[:, :-1]
+    y = data[:, -1]
+    x = torch.tensor(x, dtype=torch.float)
+    y = torch.tensor(y, dtype=torch.long)
+    dataset = torch.utils.data.TensorDataset(x, y)
+
+    for i in range(10):
+        fold_dir = os.path.join(args.check_dir, 'MLP', 'fold_%d' % (i + 1))
+        files = os.listdir(fold_dir)
+        fold_acc = []
+        max_acc = 0
+        max_epoch = 0
+        best_model = None
+
+        for f in files:
+            if f.endswith('.pth') and f.startswith('num_'):
+                acc = float(f.split('_')[3])
+                epoch_num = int(f.split('_')[-2])
+                # further select more trained models
+                # avoid under-fitting
+                if epoch_num < 100:
+                    continue
+                if acc > max_acc:
+                    max_epoch = epoch_num
+                    max_acc = acc
+                    best_model = f
+                elif acc == max_acc:
+                    if epoch_num > max_epoch:
+                        max_epoch = epoch_num
+                        best_model = f
+
+        # use the best MLP model to further extract features
+        # from the downsampled brain imaging
+        print('extracting information with model {}'.format(fold_dir + '/' + best_model))
+
+        checkpoint = torch.load(os.path.join(fold_dir, best_model))
+        dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
+
+        args = checkpoint['args']
+        model = MultilayerPerceptron(args).to('cuda:0')
+        # load model
+        model.load_state_dict(checkpoint['net'])
+
+        model.eval()
+        feature_matrix = []
+        label = []
+        correct = 0
+        for data_x, data_y in dataloader:
+            data_x, data_y = data_x.to(args.device), data_y.to(args.device)
+            out, features = model(data_x)
+            feature_matrix += features.cpu().detach().numpy().tolist()
+            pred = (out > 0).long()
+            correct += pred.eq(data_y).sum().item()
+            label += data_y.cpu().detach().numpy().tolist()
+
+        fold_acc.append(correct / 871)
+        fold_feature_matrix = np.array(feature_matrix)
+
+        # Just avoid that you have not use the stored models
+        # This indicates the final performance on test set to some degree
+        if args.verbose:
+            print('Overall accuracy: {:.6f} on fold {:d}'.format(sum(fold_acc), i + 1))
+
+        features = pd.DataFrame(fold_feature_matrix)
+        features['label'] = label
+
+        # save the features to data_dir/MLP/fold_
+        feature_path = os.path.join(args.data_dir, 'Further_Learned_Features', 'fold_%d' % (i + 1))
+        if not os.path.exists(feature_path):
+            os.makedirs(feature_path)
+        features.to_csv(os.path.join(feature_path, 'features.txt'), header=False, index=False, sep='\t')
+
+        # inherit the test indices from model path to the data path
+        shutil.copyfile(os.path.join(fold_dir, 'test_indices.txt'),
+                        os.path.join(feature_path, 'test_indices.txt'))
+
+    print('Done!')
+    print('Further Learned Features saved to features.txt')
+
+
+def test_gcn(loader, model, args):
+    """
+    Test the GCN performance on loader. We have not use validation set in GCN.
+    So, this is used to print the performance on test set
+    :param loader: an instance of torch_geometric.data.Dataloader
+    :param model: an instance of GCN
+    :param args: args from main.py
+    :return: accuracy, loss, predictions on test set
+    """
+    model.eval()
+    correct = 0.0
+    loss_test = 0.0
+    output = []
+    for data in loader:
+        data = data.to(args.device)
+        out, _ = model(data.x, data.edge_index, data.edge_attr)
+        output += out.cpu().detach().numpy().tolist()
+        pred = (out[data.test_mask] > 0).long()
+        correct += pred.eq(data.y[data.test_mask]).sum().item()
+        criterion = nn.BCEWithLogitsLoss()
+        loss_test += criterion(out[data.test_mask], data.y[data.test_mask].float()).item()
+    return correct / data.test_mask.sum().item(), loss_test, output
+
+
+def train_gcn(dataloader, model, optimizer, save_path, args):
+    """
+    Training phase of GCN. No validation set is used here.
+    :param save_path: working path for this progress
+    :param dataloader: dataloader of training set
+    :param model: an instance of GCN
+    :param optimizer: Adam, by default
+    :param args: args from main.py
+    :return: filename of the best model
+    """
+    min_loss = 1e10
+    patience_cnt = 0
+    loss_set = []
+    acc_set = []
+    best_epoch = 0
+    num_epoch = 0
+
+    t = time.time()
+    model.train()
+    for epoch in range(args.epochs):
+        loss_train = 0.0
+        correct = 0
+        num_epoch += 1
+        for i, data in enumerate(dataloader):
+            optimizer.zero_grad()
+            data = data.to(args.device)
+            out, _ = model(data.x, data.edge_index, data.edge_attr)
+            criterion = nn.BCEWithLogitsLoss()
+            loss = criterion(out[data.train_mask], data.y[data.train_mask].float())
+            loss.backward()
+            optimizer.step()
+
+            loss_train += loss.item()
+            pred = (out[data.train_mask] > 0).long()
+            correct += pred.eq(data.y[data.train_mask]).sum().item()
+
+        acc_train = correct / data.train_mask.sum().item()
+
+        if args.verbose:
+            print('\r', 'Epoch: {:06d}'.format(epoch + 1), 'loss_train: {:.6f}'.format(loss_train),
+                  'acc_train: {:.6f}'.format(acc_train), 'time: {:.6f}s'.format(time.time() - t), flush=True, end='')
+
+        loss_set.append(loss_train)
+        acc_set.append(acc_train)
+        model_state = {'net': model.state_dict(), 'args': args}
+        torch.save(model_state, os.path.join(save_path, '{}.pth'.format(epoch)))
+        if loss_set[-1] < min_loss:
+            min_loss = loss_set[-1]
+            best_epoch = epoch
+            patience_cnt = 0
+        else:
+            patience_cnt += 1
+
+        if patience_cnt == args.patience:
+            break
+
+        files = [f for f in os.listdir(save_path) if f.endswith('.pth')]
+        for f in files:
+            if f.startswith('fold'):
+                continue
+            epoch_nb = int(f.split('.')[0])
+            if epoch_nb != best_epoch:
+                os.remove(os.path.join(save_path, f))
+
+    if args.verbose:
+        print('\nOptimization Finished! Total time elapsed: {:.6f}'.format(time.time() - t))
+
+    return best_epoch
